9767b1d928b863f919a617b7f62b27de42505564
Cesar R <crojas22@comcast.net>
2018-01-15 21:36:21 -0500

    Add home page to app

--- src/main/resources/js/component/Home.js
+++ src/main/resources/js/component/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BtnLink } from "./reusable/Buttons";
+import { connect } from "react-redux";
 
-const Home = () => {
+const Home = ({auth}) => {
     return(
-        <div>
-            Home
+        <div className="homepage">
+            <div>
+                <h1>Manage events like never before</h1>
+                <BtnLink to={auth ? "/calendar" : "/login"} title={auth ? "My Calendar" : "Log in"} classes="btn-primary "/>
+            </div>
         </div>
     )
 };
 
-export default Home;
+const mapStateToProps = state => {
+    return {
+        auth: state.userAuthorized
+    }
+};
+
+export default connect(mapStateToProps)(Home);
