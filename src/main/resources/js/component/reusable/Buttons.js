ba3f1ef7f41dfa485f26f1eff4883c5d84408e2f
Cesar R <crojas22@comcast.net>
2018-01-15 21:37:43 -0500

    Add reusable button for links

--- src/main/resources/js/component/reusable/Buttons.js
+++ src/main/resources/js/component/reusable/Buttons.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export const BtnInput = ({title, classes, onClick, type}) => (
     <button type={type} className={'btn rounded-0 ' + classes} onClick={onClick}>
@@ -16,4 +17,10 @@ export const BtnSubmit = ({title, classes}) => (
     <button type='submit' className={'btn rounded-0 ' + classes}>
         {title}
     </button>
+);
+
+export const BtnLink = ({title, classes, to}) => (
+    <Link to={to} className={'btn rounded-0 ' + classes}>
+        {title}
+    </Link>
 );
